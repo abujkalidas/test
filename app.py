e1da73cf6a6d585440fd4bf3c4af91cbec50b186
Kalidas Abuj <abujkalidas@gmail.com>
2022-12-08 15:22:10 +0530

    Flask application added

--- app.py
+++ app.py
@@ -1,1 +1,13 @@
 # This is a app module
+import pandas as pd
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Default API'
+
+if __name__ == '__main__':
+    app.run(debug=True)
