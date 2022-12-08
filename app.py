b79105bd4a77fae4a72e4b09009896fe448f867f
Kalidas Abuj <abujkalidas@gmail.com>
2022-12-08 15:38:55 +0530

    second API added

--- app.py
+++ app.py
@@ -4,10 +4,15 @@ import pandas as pd
 from flask import Flask
 
 app = Flask(__name__)
-
+  
 @app.route('/')
 def index():
     return 'Default API'
+
+
+@app.route('/login')
+def login():
+    return 'Login API'
 
 if __name__ == '__main__':
     app.run(debug=True)
